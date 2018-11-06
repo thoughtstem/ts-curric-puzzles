88883abd721d40679fccbea821ce5c58b2b989ad
Sara Lucchini <saralucchini@Stephens-MacBook-Air-5.local>
2018-11-05 17:09:53 -0800

    pictures-level-1 questions

--- pictures-level-1.rkt
+++ pictures-level-1.rkt
@@ -8,6 +8,8 @@
 
 (require (prefix-in p: pict/code))
 
+(define example-color
+  (random-choose 'orange 'yellow 'green 'blue 'purple))
 
 (define (random-color)
   (random-choose 'red 'orange 'yellow 'green 'blue 'purple))
@@ -21,6 +23,46 @@
   (list
 
    #;(yes-no-question "Are you ready to code some pictures???"   'yes)
+
+   ;(yes-no-question "Let's code some pictures!"  'yes)
+
+   (yes-no-question (stack "Take a look at this code:"
+                           (code (circle 30 'solid 'red))
+                           (row "Then type " (code (yes)) " to continue."))
+                    'yes)
+
+   (yes-no-question (stack "The code you just saw will make a..."
+                           (code circle)
+                           (row "with a radius of " (code 30) " pixels")
+                           (row "a mode of " (code 'solid))
+                           (row "and a color of " (code 'red))
+                           (row "Type " (code (yes)) " to continue!"))
+                    'yes)
+   
+   (simple-question (stack "And here's that circle!"
+                           (circle 30 'solid 'red)
+                           "Now you try."
+                           "Type the code to make this circle."
+                           "(you can look at the code by scrolling up!)"
+                           "When you get the code exactly right,"
+                           (row "type " (code (answer___)))
+                           "with the circle code in that blank.")
+                    (curry equal?
+                           (circle 30 'solid 'red)))
+
+   (yes-no-question (stack "Pretty cool!"
+                           "But there are more colors than just red, right?")
+                    'yes)
+
+   (simple-question (stack "I challenge you!"
+                           (row "Can you code a " (code circle))
+                           "with the same size and mode"
+                           (row "but make it " (code #,example-color) "?")
+                           (row "Type your answer inside an " (code (answer ____)))
+                           "just like before.")
+                    (curry equal?
+                           (circle 30 'solid example-color)))
+
 
    (simple-question (stack "Read this code..."
                            "Pick 'a 'b or 'c"
