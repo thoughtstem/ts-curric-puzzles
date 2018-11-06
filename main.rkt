66fe85289003059e6e7093b8c0116ea4e14e7395
srfoster <stephen@thoughtstem.com>
2018-11-06 12:22:56 -0800

    new puzzle: pictures-level-1

--- main.rkt
+++ main.rkt
@@ -7,6 +7,11 @@
 
 (require "./quizzes.rkt")
 (require "./typing-game.rkt")
+(require "./pictures-level-1.rkt")
+(require "./pictures-level-2.rkt")
+
 (provide
  (all-from-out "./quizzes.rkt")
- (all-from-out "./typing-game.rkt"))
+ (all-from-out "./typing-game.rkt")
+ (all-from-out "./pictures-level-1.rkt")
+ (all-from-out "./pictures-level-2.rkt"))
