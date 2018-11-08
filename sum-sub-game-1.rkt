0496a9f9c48a9833b36f30f648efa62191cbc533
elenaalexandrova8 <sandiego1088@gmail.com>
2018-11-08 14:59:25 -0800

    updates

--- sum-sub-game-1.rkt
+++ sum-sub-game-1.rkt
@@ -12,47 +12,50 @@
   (try (circle params ...)))
 
 ; sum-sub-game-1
-; Addition and substraction.
+; Addition and subtraction.
 
 (define qs
   (list
    (yes-no-question (stack
-                     "Let’s practice adding and substracting numbers in Dr. Racket!"
+                     "Let’s practice adding and subtracting numbers in Dr. Racket!"
                      "But first, let me show you how to type your answer in this game."
                      (row "Please type " (code (yes)) " to proceed.")) 'yes)
 
    (yes-no-question (stack
                      "In this game sometimes you will need"
                      (row "to type your answer as " (code (yes)) " or " (code (no))".")
-                     "Please type (yes) to proceed.") 'yes)
+                     (row "Please answer " (code (no)) " to continue.")) 'no)
 
    (simple-question (stack
-                     "For some questions you'll need to type in Dr. Racket commands: "
+                     "For some questions you'll need to type in Dr. Racket command: "
                      (row (code (answer _____))", where the blank field is the command.")
                      "Let's practice that!" 
                      "Please answer this question by typing the command to draw a circle:"
                      (code (answer (circle 20 "solid" "blue"))))
                     (curry equal? (circle 20 "solid" "blue")))
 
+   (simple-question (stack
+                     "Great job! Let's play with some numbers!"
+                     "I’m sure you know how to add two numbers together."
+                     "What is 2 + 3 equal to?"
+                     (row "Please use " (code (answer _____)) "."))
+                    (curry equal? 5))
 
    (yes-no-question (stack
-                     "Great job! Let's play with some numbers!"
-                     "I’m sure you know how to add two numbers together, for example:"
-                     "2+3 = 5."
-                     "To do that in Dr. Racket we need to run following command:"
+                     "That's right!"
+                     "To do 2 + 3 in Dr.Racket we need to run following command:"
                      (code (+ 2 3))
-                     "Please type (yes) to proceed.")
+                     (row "Please type " (code (yes)) " to proceed."))
                     'yes)
    
    (simple-question (stack
-                     (row "So, if a command to do 2+3 in Dr. Racket is "(code (+ 2 3)))
-                     "what will be the command to sum numbers 1 and 4?"
-                     (row "Please use the form of answer " (code (answer _____)) " and replace the blank field")
-                     (row "with your command, for example:" (code (answer (+ 2 3))) "."))
+                     "Now, what will be the command to add numbers 1 and 4?"
+                     (row "Please use " (code (answer _____)) " and replace the blank field")
+                     (row "with your command, for example: " (code (answer (+ 2 3))) "."))
                     (curry equal? (or (+ 1 4) (+ 4 1))))
 
    (simple-question (stack
-                     "Now, what if you need to add numbers 12 and 7 in Dr. Racket? "
+                     "What if you need to add numbers 12 and 7 in Dr. Racket?"
                      "Please type in the command to do that."
                      (row "HINT: use " (code (answer _____)) " to type your answer again."))
                     (curry equal? (or (+ 12 7) (+ 7 12))))
@@ -60,62 +63,47 @@
    
    (simple-question (stack
                      "And one more example to practice:"
-                     "what will be the command to add 67 and 32?"
-                     (row "HINT: use " (code (answer _____)) " to type your answer again."))
+                     "what will be the command to add 67 and 32?")
                     (curry equal? (or (+ 67 32) (+ 32 67))))
 
    (yes-no-question (stack
-                     "Also, using addition command in Dr. Racket"
-                     "you can add many numbers together: "
+                     "Also, in Dr.Racket you can add many numbers together: "
                      (code (+ 7 1 3))
                      "This command is equivalent to 7+1+3."
-                     "Please type (yes) to continue.")
+                     (row "Please answer " (code (yes)) " to continue."))
                     'yes)
 
    (simple-question (stack
-                     (row "Now, this creature " (scale 0.3 (random-dude))" was born in 2011.")
-                     "Can you type the command to sum all digits in it's birth year?" 
+                     (row "This creature " (scale 0.3 (random-dude))" was born in 2011.")
+                     "Can you type the command to add all four single digits in it's birth year?" 
                      (row "HINT: please use " (code (answer _____)) " again and keep order of digits."))
                     (curry equal? (+ 2 0 1 1)))
 
 
    (yes-no-question (stack
                      "You doing great!"
-                     "Let's try to substract numbers!"
-                     "To substract two numbers, let's say 3 from 10:"
-                     "10-3=7"
-                     "in Dr. Racket you would need to use following command:"
+                     "Subtraction command looks very simular to addition:"
                      (code (- 10 3))
-                     "Looks very simular to addition, right?"
-                     "Please answer (yes) to continue.")
+                     (row "Please answer " (code (yes)) " to continue."))
                     'yes)
 
    (simple-question (stack
-                     (row "Now, a Dr. Racket command to do 10-3 is " (code (- 10 3)))
+                     "Try to do subtraction command yourself."
                      "What will be a command to do 12-7?"
                      (row "HINT: please use " (code (answer _____)) " here."))
                     (curry equal? (- 12 7)))
 
    (yes-no-question (stack
                      "Awesome!"
-                     "Remember how we could add several numbers in one command?"
-                     (row "Like command to do 1+2+3+5 will be: " (code (+ 1 2 3 5)))
-                     "You can substract many numbers in one comand too!"
+                     "You can subtract many numbers in one comand too!"
                      (row "For example, a command to do 10-2-5 looks like: " (code (- 10 2 5)))
-                      "Please answer (yes) to continue.")
+                     (row "Please answer " (code (yes)) " to continue."))
                     'yes)
 
    (simple-question (stack
-                     "Now, try substracting several numbers in one command yourself!"
-                     "Please type the command to substract 50-3-21."
-                     (row "HINT: don't forget to use " (code (answer _____)) " for your answer."))
+                     "Now, please type the command to subtract 50-3-21."
+                     (row "HINT: please use " (code (answer _____)) " here."))
                     (curry equal? (or (- 50 3 21) (- 50 21 3))))
-
-   (yes-no-question (stack
-                     "You've made an incredible progress!"
-                     "Thank you for playing this puzzles!"
-                     "Please type (yes) to finish!")
-                    'yes)
 ))
 
 (define (the-details-game)
--- sum-sub-game-2.rkt
+++ sum-sub-game-2.rkt
@@ -12,18 +12,17 @@
   (try (circle params ...)))
 
 ; sum-sub-game-2
-; Order of operations with addition and substraction.
+; Order of operations with addition and subtraction.
 (define qs
   (list
    (yes-no-question (stack
-                     "Let’s practice adding and substracting numbers in Dr. Racket!"
-                     "But first, let me show you how to type your answer in this game."
+                     "Let’s practice adding and subtracting numbers in Dr. Racket!"
                      (row "Please type " (code (yes)) " to proceed.")) 'yes)
 
    (yes-no-question (stack
                      "In this game sometimes you will need"
                      (row "to type your answer as " (code (yes)) " or " (code (no))".")
-                     "Please type (yes) to proceed.") 'yes)
+                     (row "Please type " (code (yes)) " to proceed.")) 'yes)
 
    (simple-question (stack
                      "For some questions you'll need to type in Dr. Racket commands: "
@@ -42,7 +41,7 @@
                      "This is equvalent to:"
                      "2 + 3 = 5"
                      "34 + 5 + 6 + 3 = 48"
-                     "Please type (yes) to proceed.")
+                    (row "Please type " (code (yes)) " to proceed."))
                     'yes)
    
    (simple-question (stack
@@ -51,8 +50,7 @@
                     (curry equal? (or (+ 5 1) (+ 1 5))))
 
    (yes-no-question (stack
-                     "Looks great! Let's refresh how to substract numbers in Dr. Racket!"
-                     "You can use operator - to substract two and more numbers:"
+                     "Very good! Here is subtraction commands:"
                      (code (- 2 1))
                      (code (- 30 5 1))
                      "This is equvalent to:"
@@ -68,24 +66,22 @@
 
    
    (yes-no-question (stack
-                     "Wonderful! You can add and substract numbers in Dr. Racket like a pro!"
-                     "That's fantastic! Hope you're ready to move on!"
+                     "Wonderful! You can add and subtract numbers in Dr. Racket like a pro!"
                      "Next we will add more parenthesis in our commands:)"
-                     "Please type (yes) to proceed.")
+                     (row "Please type " (code (yes)) " to proceed."))
                     'yes)
 
    (yes-no-question (stack
-                     "Now, sometimes in math we use parenthesis ()"
-                     "to show what operation will be done first."
+                     "In math we use parenthesis () to show what operation will be done first."
                      "For example:"
-                     "(4 – 2) + 1 = 3"
-                     "here first we would do 4-2=2, and then add 1: 2+1=3."
-                     "Please type (yes) to continue.")
+                     "(4 – 2) + 1"
+                     "2 + 1"
+                     "3"
+                     (row "Please type " (code (yes)) " to proceed."))
                     'yes)
 
    (yes-no-question (stack
-                     "Good news is that we use parentheis in Dr. Racket"
-                     "the same way we do at math!"
+                     "In Dr.Racket code in inner parenthesis will be run first."
                      "For example, the command to do:"
                      "(4 – 2) + 1 = 3"
                      "looks like this:"
@@ -93,22 +89,17 @@
                      "code in inner parenthesis will be run first:"
                      (code (- 4 2))
                      "and then 1 will be added to the result of that."
-                     "Please type (yes) to continue.")
+                     (row "Please type " (code (yes)) " to proceed."))
                     'yes)
 
    (yes-no-question (stack
-                     "Now we will try to type Dr.Racket command "
-                     "to compute expression with parenthesis."
-                     "Let's look on this expression:"
+                     "Now, let's look on this expression:"
                      "5 – (3 + 1)"
                      "Do we need to do addition (+) first to solve this?"
-                     "Please answer (yes) or (no).")
+                     (row "Please type " (code (yes)) " to proceed."))
                     'yes)
 
    (simple-question (stack
-                     "Great! Yes, to solve an expression:"
-                     "5 – (3 + 1)"
-                     "we first need to do 3+1, because it's in parenthesis."
                      "Please type in the command to do 3+1 in Dr. Racket."
                      (row "HINT: please use " (code (answer _____)) " to type your command."))
                     (curry equal? (or (+ 3 1) (+ 1 3))))
@@ -118,7 +109,7 @@
                      "5 – (3 + 1)"
                      "we first need to do 3+1:"
                      (code (+ 3 1 ))
-                     "Then we need to substract that from 5:"
+                     "Then we need to subtract that from 5:"
                      (code (- 5 ___))
                      (row "Please replace ___ with " (code (+ 3 1 )) "to finish the command.")
                      (row "HINT: please use " (code (answer _____)) " to type your command.")
@@ -133,17 +124,8 @@
                      "Let's try simular expression:"
                      "23 – (10 + 5)"
                      "Please type a Dr. Racket command to compute it."
-                     (row "HINT: please use " (code (answer _____)) " to type your command.")
-                     (row "HINT-2: expressions  "  "5 – (3  + 1)")
-                     (row "                                "  "23 – (10 + 5)")
-                     "look very simular right? Only numbers are different.")
+                     (row "HINT: don't forget to use " (code (answer _____)) "."))
                     (curry equal? (or (- 23 (+ 10 5)) (- 23 (+ 5 10)))))
-
-   (yes-no-question (stack
-                     "Wow, that was great!"
-                     "Thank you for playing this puzzles!"
-                     "Please type (yes) to finish!")
-                    'yes)
 ))
 
 (define (the-details-game)
